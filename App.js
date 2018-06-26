ca56f5adcc429aaabeb0d560bc30d17c9751ad22
Caleb <savershot685@gmail.com>
2018-06-26 15:54:12 -0500

    State and Switches are cool!
    Now if you are intimidated by my youthfull picture you can turn it off!

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Switch, } from 'react-native';
 var S = require('./aParts/style');
 import Home from './aParts/homeF/home';
 import About from './aParts/aboutF/about';
 import Projects from './aParts/projectsF/projects';
 
 export default class App extends React.Component {
-  render() {
+ constructor(){
+     super();
+     this.state={
+         meHiddenHead: true
+     }
+ }
+    
+    render() {
+    let meHiddenHead = this.state.meHiddenHead;
+    let img = <Text></Text>;
+    if(!meHiddenHead){
+        img = <Text></Text>;
+    }else{
+        img =<Image style ={S.image} source ={require('./graphics/mehead.jpg')}/>;
+    }
     return (
       <View>
       <ScrollView style ={S.scrollView}>
-        <View style={S.imageC}>   <Image style ={S.image} source ={require('./graphics/mehead.jpg')}/> </View>
-          
+        <View style={S.imageC}>
+        {img}
+        <Switch onValueChange={(value) => this.setState({meHiddenHead: value})} value ={this.state.meHiddenHead}/>       
+        </View>       
         <Text style = {S.meMe}> Caleb Campbell</Text>
         <Home/>
         <About/>
