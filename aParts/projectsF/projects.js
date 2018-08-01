8f4c38addcf66ad6622b89ed5e320c0c0671cfc5
Caleb <savershot685@gmail.com>
2018-07-31 20:17:02 -0500

    Added a paragraph to the projects section

--- aParts/projectsF/projects.js
+++ aParts/projectsF/projects.js
@@ -5,7 +5,12 @@ export default class Projects extends React.Component {
   render() {
     return (
       <View>
-        <Text style={S.lesserH}>My projects</Text>
+        <Text style={S.heading}>My projects</Text>
+        <Text> There is art in my blood, I am pretty sure I got that from my father. Unlike my father, I express my artistic side digitally.
+          It has taken many forms over the years; It has gone from graphic arts to visual media and almost anything anyone can think of. Honestly, I think
+          it skipped musically. I did try to play the clarinet back in the seventh grade; I am fairly certain I cannot keep time to save my life, musically.
+          </Text>
+        <Text style={S.lesserH}>Web sites to visit</Text>  
         <Text style={S.pLink}
         onPress={()=>Linking.openURL('http://omegamannt.deviantart.com')}>My Deviant Art page</Text>
         <Text style={S.pLink}
